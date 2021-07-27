18bb41028a1465f7a14e9fd6d524207dfa491b54
Aleks1922 <merinovAleks46@yandex.ru>
2021-07-27 20:17:30 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -1,69 +1,100 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  }
-});
-
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-
-});
-
-ymaps.ready(init);
-function init() {
-  var apiMaps = new ymaps.Map("api-maps", {
-    center: [25.07, 55.13],
-    zoom: 13
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    }
   });
-}
 
-let menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
-  document.body.classList.toggle('lock')
-})
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
 
-// Отправка данных на сервер
-function send(event, php) {
-  console.log("Отправка запроса");
-  event.preventDefault ? event.preventDefault() : event.returnValue = false;
-  var req = new XMLHttpRequest();
-  req.open('POST', php, true);
-  req.onload = function () {
-    if (req.status >= 200 && req.status < 400) {
-      json = JSON.parse(this.response); // Ебанный internet explorer 11
-      console.log(json);
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
 
-      // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
-      if (json.result == "success") {
-        // Если сообщение отправлено
-        alert("Сообщение отправлено");
-      } else {
-        // Если произошла ошибка
-        alert("Ошибка. Сообщение не отправлено");
-      }
-      // Если не удалось связаться с php файлом
-    } else { alert("Ошибка сервера. Номер: " + req.status); }
-  };
+  });
 
-  // Если не удалось отправить запрос. Стоит блок на хостинге
-  req.onerror = function () { alert("Ошибка отправки запроса"); };
-  req.send(new FormData(event.target));
-}
+  ymaps.ready(init);
+  function init() {
+    var apiMaps = new ymaps.Map("api-maps", {
+      center: [25.07, 55.13],
+      zoom: 13
+    });
+  }
+
+  let menuButton = document.querySelector('.menu-button');
+  menuButton.addEventListener('click', function () {
+    document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
+    document.body.classList.toggle('lock')
+  })
+
+  // Отправка данных на сервер
+  function send(event, php) {
+    console.log("Отправка запроса");
+    event.preventDefault ? event.preventDefault() : event.returnValue = false;
+    var req = new XMLHttpRequest();
+    req.open('POST', php, true);
+    req.onload = function () {
+      if (req.status >= 200 && req.status < 400) {
+        json = JSON.parse(this.response); // Ебанный internet explorer 11
+        console.log(json);
+
+        // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+        if (json.result == "success") {
+          // Если сообщение отправлено
+          alert("Сообщение отправлено");
+        } else {
+          // Если произошла ошибка
+          alert("Ошибка. Сообщение не отправлено");
+        }
+        // Если не удалось связаться с php файлом
+      } else { alert("Ошибка сервера. Номер: " + req.status); }
+    };
+
+    // Если не удалось отправить запрос. Стоит блок на хостинге
+    req.onerror = function () { alert("Ошибка отправки запроса"); };
+    req.send(new FormData(event.target));
+  }
+
+  var modalButton = $('[data-toggle=modal]')
+  var closeModalButton = $('.modal__close')
+  modalButton.on('click', openModal)
+  closeModalButton.on('click', closeModal)
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+    document.body.classList.add('lock');
+  }
+  function closeModal(evt) {
+    evt.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+    document.body.classList.remove('lock');
+  }
+
+  function ESCclose(evt) {
+    if (evt.keyCode == 27) {
+      //window.close();
+      console.log('close the window...')
+    }
+  }
+
+});
